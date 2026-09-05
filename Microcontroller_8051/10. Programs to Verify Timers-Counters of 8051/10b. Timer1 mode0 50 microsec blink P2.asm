056716aa22be14e5cc2ca52833f1b40b9aafeb6c
emericreaper75 <blazingrecquaza7641@outlook.com>
2026-09-05 15:02:40 +0000

    Refactor Timer1 mode 0 program for 50us blink
    Updated Timer1 mode 0 program to blink P2 with a 50 microsecond delay using a 13-bit timer count.

--- Microcontroller_8051/10. Programs to Verify Timers-Counters of 8051/10b. Timer1 mode0 50 microsec blink P2.asm
+++ Microcontroller_8051/10. Programs to Verify Timers-Counters of 8051/10b. Timer1 mode0 50 microsec blink P2.asm
@@ -1,14 +1,23 @@
-; Experiment 10b: Timer1 mode 0, approximately 50 microseconds, blink P2
-ORG 0000H
-    MOV TMOD, #00H
-LOOP10B:
-    MOV TH1, #0FCH
-    MOV TL1, #018H
-    SETB TR1
-WAIT10B:
-    JNB TF1, WAIT10B
-    CLR TR1
-    CLR TF1
-    CPL P2
-    SJMP LOOP10B
-END
+; ---------------------------------------------------------
+; 50 us delay using Timer1 in Mode 0 (13-bit timer)
+; Blinks all pins of Port 2
+; 13-bit count for 50 us @ 12MHz = 8192 - 50 = 8142 = 1FCEH
+; TL1 gets lower 5 bits, TH1 gets upper 8 bits
+; ---------------------------------------------------------
+ORG 0000H                ; reset vector: execution starts here
+    LJMP MAIN              ; jump to MAIN, skipping interrupt vector area
+
+ORG 0030H                ; main code placed after interrupt vector table
+MAIN:                     ; label: start of main program
+    MOV TMOD,#00H           ; TMOD=00H -> Timer1 selected, Mode 0 (13-bit)
+AGAIN:                     ; label: start of repeating blink loop
+    MOV TH1,#0FEH            ; load high 8 bits of the 13-bit count (FEH)
+    MOV TL1,#00EH            ; load low 5 bits of the 13-bit count (0EH)
+    SETB TR1                 ; start Timer1 running
+WAIT:                       ; label: wait for timer overflow
+    JNB TF1,WAIT              ; loop here while overflow flag TF1 is still 0
+    CLR TR1                  ; stop Timer1 once it has overflowed
+    CLR TF1                  ; clear overflow flag for the next round
+    CPL P2                   ; complement (toggle) all 8 pins of Port 2
+    SJMP AGAIN                ; repeat forever, creating a blinking effect
+    END                      ; end of source file
